ac8a3cc9f6d9097c8799b07d2c45e0d145befd13
Natalie Halawani <hala1224@gmail.com>
2018-10-29 01:00:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function BoardMember(name, homeState, training) {
     return 'You will see great things from Scuber.';
   };
   this.sayHi = function () {
-    str ="Hi, my name is " + this.name + '. I am from ' + this.homeState + ' and I was trained in ' + this.training +'.';
+    str ="Hi, my name is " + this.name + '. I am from ' + this.homeState + ', and I was trained in ' + this.training +'.';
     return str;
     
   };
